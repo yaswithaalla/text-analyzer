a98965d9e1ee66db1e26e323f3282366b03b59a8
yaswithaalla <147685607+yaswithaalla@users.noreply.github.com>
2025-04-14 18:51:44 +0530

    Update nltk_download.py

--- nltk_download.py
+++ nltk_download.py
@@ -1,5 +1,11 @@
 #nltk_download.py
-import nltk
-nltk.download('punkt')
+
 nltk.download('averaged_perceptron_tagger')
 nltk.download('stopwords')
+import nltk
+
+# Check if 'punkt' is already downloaded; if not, download it
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
